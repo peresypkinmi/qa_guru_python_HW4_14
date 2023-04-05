a33fbbdc245e6a670f3a72ce2953c88c3e8a668e
skhomuti <skhomuti@gmail.com>
2023-04-06 00:04:52 +0500

    Добавлены материалы после занятия

--- test_parametrize.py
+++ test_parametrize.py
@@ -3,34 +3,61 @@ from dataclasses import dataclass
 import pytest
 
 
+@pytest.mark.parametrize("browser", ["Chrome", "Firefox", "Safari"])
 def test_with_param(browser):
     pass
 
 
+@pytest.mark.parametrize("os", ["ubuntu-22.04", "windows-11", "macos-13.2"],
+                         ids=["Ubuntu", "Windows", "MacOS"])
+@pytest.mark.parametrize("group_id", [125961245, 94816283, 129846712],
+                         ids=["Admin", "User", "Support"])
+def test_with_os(os, group_id):
+    pass
+
+
+@pytest.mark.parametrize("browser", ["Chrome", "Firefox", "Safari"])
+@pytest.mark.parametrize("test_user", ["Admin", "User", "Support"])
 def test_with_matrix_param(browser, test_user):
     pass
 
 
+@pytest.mark.parametrize("browser", [
+    pytest.param("Chrome"),
+    pytest.param("Firefox", marks=[pytest.mark.xfail(reason="TASK-123")]),
+    pytest.param("Safari", marks=[pytest.mark.slow])
+])
 def test_with_param_marks(browser):
     pass
 
 # ---------------------------------------------
 
 
+@pytest.fixture(params=["Chrome", "Firefox", "Safari"])
 def browser(request):
-    pass
+    print("Фикстура вызвалась с параметром " + request.param)
+    if request.param == "Chrome":
+        return ""
+    if request.param == "Firefox":
+        return ""
+    if request.param == "Safari":
+        return ""
 
 
 def test_with_parametrized_fixture(browser):
     pass
 
 
+@pytest.mark.parametrize("browser", ["Chrome", "Firefox"], indirect=True)
 def test_with_indirect_parametrization(browser):
     pass
 
 
-# @common_user
-def test_with_account(test_user):
+chrome_only = pytest.mark.parametrize("browser", ["Chrome"], indirect=True)
+
+
+@chrome_only
+def test_with_account(browser):
     pass
 
 
@@ -42,13 +69,17 @@ class User:
     description: str
 
     def __repr__(self):
-        return f"{self.name} ({self.id})"
+        return f"User: {self.id}, {self.name}"
 
 
 user1 = User(id=1, name="Mario", age=32, description="something " * 10)
 user2 = User(id=2, name="Wario", age=62, description="else " * 10)
 
 
-@pytest.mark.parametrize("user", [user1, user2])
+def show_user(user):
+    return f"User: {user.id}, {user.name}"
+
+
+@pytest.mark.parametrize("user", [user1, user2], ids=repr)
 def test_users(user):
     print()
